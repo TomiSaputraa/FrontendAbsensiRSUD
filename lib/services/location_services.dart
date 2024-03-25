b768b237ea165635a4bb2775a7dac994c0042ed7
TomiSaputraa <tommycodingjj@gmail.orgg>
2024-03-25 15:00:12 +0700

    add : location mock detection

---
+++ lib/services/location_services.dart
@@ -0,0 +1,27 @@
+import 'package:geolocator/geolocator.dart';
+
+import '../utils/ui_utils.dart';
+
+class LocationServices {
+  // Handle permission
+  Future<bool> handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return false;
+    }
+    return true;
+  }
+}
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:absensi_mattaher/ui/styles/colors.dart';
 import 'package:absensi_mattaher/utils/ui_utils.dart';
 import 'package:flutter/material.dart';
@@ -5,7 +7,9 @@ import 'package:absensi_mattaher/repositories/user_repositories.dart';
 import 'package:absensi_mattaher/ui/screens/login.dart';
 import 'package:absensi_mattaher/ui/screens/user/home.dart';
 import 'package:absensi_mattaher/model/user_model.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:nb_utils/nb_utils.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -28,7 +32,76 @@ class _SplashScreenState extends State<SplashScreen> {
         statusCode = error;
       });
     });
+    detectMockLocation();
     init();
+  }
+
+  // Handle permission
+  Future<bool> handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      UiUtils.setSnackbar(context,
+          text: "Lokasi tidak di aktifkan, mohon aktifkan!");
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        UiUtils.setSnackbar(context, text: "Permintaan lokasi ditolak");
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      UiUtils.setSnackbar(context,
+          text: "Lokasi ditolak permanen, mohon hidupkan izin lokasi");
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> detectMockLocation() async {
+    final hasPermission = await handleLocationPermission();
+    if (!hasPermission) {
+      UiUtils.setSnackbar(context, text: "Izin lokasi ditolak");
+      return;
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    // Deteksi fake GPS
+    if (position.isMocked) {
+      // Inisialisasi plugin push notification
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+          FlutterLocalNotificationsPlugin();
+
+      // Konfigurasi untuk Android
+      var initializationSettingsAndroid =
+          const AndroidInitializationSettings('@mipmap/ic_launcher');
+      var initializationSettings =
+          InitializationSettings(android: initializationSettingsAndroid);
+      await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+      // Tampilkan push notification sebelum keluar
+      var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+          'channel_id', 'Notifikasi Absensi',
+          importance: Importance.max, priority: Priority.high);
+      var platformChannelSpecifics =
+          NotificationDetails(android: androidPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.show(
+        0,
+        'Peringatan',
+        'Anda menggunakan lokasi palsu. Aplikasi akan keluar.',
+        platformChannelSpecifics,
+      );
+
+      await Future.delayed(const Duration(seconds: 2));
+      exit(0);
+    }
   }
 
   Future<void> init() async {
